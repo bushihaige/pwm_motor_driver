e0cd253c9849a3b131966941251fa8e70a244655
LHL <841907101@qq.com>
2025-11-12 22:49:56 +0800

    加入了给定占空比下通过gpio输出pwm波的函数simple_pwm

--- Core/Src/user_workspace.c
+++ Core/Src/user_workspace.c
@@ -1,9 +1,29 @@
 #include "user_workspace.h"
 #include "usart.h"
 #include "gpio.h"
+#include "stm32f4xx.h"   // 确保有 DWT 寄存器定义
+
 
 #define PI 3.1415926f
-#define PWM_MAX 999  
+#define PWM_MAX 999
+
+#define PWM_RES 100
+#define DELAY_US 100
+#define DUTY_CYCLE 0.2
+
+void DWT_Init(void)
+{
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CYCCNT = 0;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+}
+
+void delay_us(uint32_t us)
+{
+    uint32_t start = DWT->CYCCNT;
+    uint32_t ticks = us * (SystemCoreClock / 1000000);
+    while ((DWT->CYCCNT - start) < ticks);
+}
 
 static void SinPWM_Update(void)
 {
@@ -23,37 +43,43 @@ static void SinPWM_Update(void)
         theta -= 2.0f;
 }
 
+void simple_drive_motor(void){
+	// 三相GPIO切换测试
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_Delay(500);
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_Delay(500);
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	HAL_Delay(500);
+
+}
+void fixed_pwm(void){
+    for (int i = 0; i < PWM_RES; i++) {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, (i < DUTY_CYCLE * PWM_RES) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+        delay_us(DELAY_US);
+    }
+
+}
+
+
 void User_Workspace_Init(void)
 {
-	// 如果启用 PWM，可以在此启动
-    // HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-    // HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
-    // HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-    // HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
-    // HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
-    // HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
+	DWT_Init();
     HAL_UART_Transmit(&huart1, (uint8_t*)"User Init OK\r\n", 13, HAL_MAX_DELAY);
 }
 
 
+
 void User_Workspace_Loop(void)
 {
-    // 三相GPIO切换测试
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
-    HAL_Delay(500);
-
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
-    HAL_Delay(500);
-
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
-    HAL_Delay(500);
-
-    // 若想输出正弦PWM：
-    // SinPWM_Update();
+	//simple_drive_motor();
+	fixed_pwm();
 }
